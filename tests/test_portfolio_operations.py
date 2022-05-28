ebb64710be2afb02ceeb24b9274f0c0e48f4d38a
bubuss <mikolaj.janusz123@gmail.com>
2022-05-28 19:06:04 +0200

    testing: refactor AddAsset testing class Create asset template for testing and docstring to testing functions.

--- tests/test_portfolio_operations.py
+++ tests/test_portfolio_operations.py
@@ -1,6 +1,7 @@
 import unittest
 
 from pathlib import Path
+from typing import Dict
 
 from src.controllers.portfolio_controller import PortfolioController
 
@@ -8,15 +9,24 @@ from src.controllers.portfolio_controller import PortfolioController
 class TestAddAsset(unittest.TestCase):
     def setUp(self) -> None:
         self.portfolio_controller = PortfolioController(Path("/"), "test")
-        return super().setUp()
 
-    def test_add_asset(self) -> None:
-        test_asset = {
+        self._asset_template = {
             "name": "Test",
-            "unit_price": "1",
-            "amount": "10",
+            "unit_price": 1,
+            "amount": 10,
             "currency": "USD",
         }
+
+        return super().setUp()
+
+    @property
+    def asset_template(self) -> Dict:
+        return self._asset_template.copy()
+
+    def _check_add_valid_asset(self, test_asset: Dict) -> None:
+        """Add asset to portfolio and check if it is in portfolio assets
+        and has the same values as provided in arguments.
+        """
 
         self.portfolio_controller.add_asset(
             test_asset["name"],
@@ -27,7 +37,7 @@ class TestAddAsset(unittest.TestCase):
 
         assets = self.portfolio_controller._portfolio._assets
 
-        # test if test asset is in assets
+        # test if test asset name in assets
         self.assertIn(test_asset["name"], assets.keys())
 
         asset = assets[test_asset["name"]]
@@ -36,6 +46,16 @@ class TestAddAsset(unittest.TestCase):
         self.assertEqual(asset["unit_price"], test_asset["unit_price"])
         self.assertEqual(asset["amount"], test_asset["amount"])
         self.assertEqual(asset["currency"], test_asset["currency"])
+
+    def test_add_example(self) -> None:
+        """Test portfolio controller add_asset function
+        provided valid example arguments
+
+        Should add an asset to portfolio with values provided in arguments.
+        """
+
+        test_asset = self.asset_template
+        self._check_add_valid_asset(test_asset)
 
 
 if __name__ == "__main__":
