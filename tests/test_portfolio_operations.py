1febcd9fe6df46a1d5cf54d2637959bad8d61082
bubuss <mikolaj.janusz123@gmail.com>
2022-05-28 19:11:04 +0200

    testing: Create tests for invalid input in AddAsset class
    Created tests for negative amount, unit_price also tests for empty name and currency string. Created helper function which tests for ValueError raise in add_asset function

--- tests/test_portfolio_operations.py
+++ tests/test_portfolio_operations.py
@@ -47,6 +47,27 @@ class TestAddAsset(unittest.TestCase):
         self.assertEqual(asset["amount"], test_asset["amount"])
         self.assertEqual(asset["currency"], test_asset["currency"])
 
+    def _check_add_raise_exception(
+        self, test_asset: Dict, exception: Exception
+    ) -> None:
+        """Check if portoflio add_asset raises provided exception,
+        also check if asset is not added into portfolio assets.
+        """
+
+        self.assertRaises(
+            exception,
+            self.portfolio_controller.add_asset,
+            test_asset["name"],
+            test_asset["unit_price"],
+            test_asset["amount"],
+            test_asset["currency"],
+        )
+
+        assets = self.portfolio_controller._portfolio._assets
+
+        # check if asset was not added to assets
+        self.assertNotIn(test_asset["name"], assets.keys())
+
     def test_add_example(self) -> None:
         """Test portfolio controller add_asset function
         provided valid example arguments
@@ -56,6 +77,52 @@ class TestAddAsset(unittest.TestCase):
 
         test_asset = self.asset_template
         self._check_add_valid_asset(test_asset)
+
+    def test_add_empty_name(self) -> None:
+        """Test portfolio controller add_asset function
+        provided with empty string as name argument
+
+        Should raise ValueError
+        """
+
+        test_asset = self.asset_template
+        test_asset["name"] = ""
+
+        self._check_add_raise_exception(test_asset, ValueError)
+
+    def test_add_negative_unitprice(self) -> None:
+        """Test portfolio controller add_asset function
+        provided with empty string as unit price argument
+
+        Should raise ValueError
+        """
+
+        test_asset = self.asset_template
+        test_asset["unit_price"] = -1
+
+        self._check_add_raise_exception(test_asset, ValueError)
+
+    def test_add_negative_amount(self) -> None:
+        """Test portfolio controller add_asset function
+        provided with empty string as amount argument
+
+        Should raise ValueError
+        """
+
+        test_asset = self.asset_template
+        test_asset["amount"] = -10
+        self._check_add_raise_exception(test_asset, ValueError)
+
+    def test_add_empty_currency(self) -> None:
+        """Test portfolio controller add_asset function
+        provided with empty string as currency argument
+
+        Should raise ValueError
+        """
+
+        test_asset = self.asset_template
+        test_asset["currency"] = ""
+        self._check_add_raise_exception(test_asset, ValueError)
 
 
 if __name__ == "__main__":
