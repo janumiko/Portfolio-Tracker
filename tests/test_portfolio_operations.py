83fb20b4087e4243f144f5d8a1fa4e2289d5c12a
bubuss <mikolaj.janusz123@gmail.com>
2022-05-29 02:17:34 +0200

    tests: Created TestBuyAsset class for buy asset operation tests
    Created TestBuyAsset class testing buy_asset function. Tests invalid and valid input, creation of transaction record and adding of asset

--- tests/test_portfolio_operations.py
+++ tests/test_portfolio_operations.py
@@ -206,5 +206,137 @@ class TestRemoveAsset(unittest.TestCase):
         self._initialize_example_portfolio()
 
 
+class TestBuyAsset(unittest.TestCase):
+    def setUp(self) -> None:
+        self.portfolio_controller = PortfolioController(Path("/"), "test")
+
+        self._asset_template = {
+            "name": "Test",
+            "unit_price": 1,
+            "amount": 10,
+            "currency": "USD",
+        }
+
+        return super().setUp()
+
+    @property
+    def asset_template(self) -> Dict:
+        return self._asset_template.copy()
+
+    def _clear_transactions(self) -> None:
+        self.portfolio_controller._portfolio._transactions = []
+
+    def _check_buy_valid_asset(self, test_asset: Dict) -> None:
+        """buy asset to portfolio and check if it is in portfolio assets
+        and has the same values as provided in arguments.
+        """
+
+        self.portfolio_controller.buy_asset(
+            test_asset["name"],
+            test_asset["unit_price"],
+            test_asset["amount"],
+            test_asset["currency"],
+        )
+
+        assets = self.portfolio_controller._portfolio._assets
+
+        # test if test asset name in assets
+        self.assertIn(test_asset["name"], assets.keys())
+
+        asset = assets[test_asset["name"]]
+
+        # test if the values are the same
+        self.assertEqual(asset["unit_price"], test_asset["unit_price"])
+        self.assertEqual(asset["amount"], test_asset["amount"])
+        self.assertEqual(asset["currency"], test_asset["currency"])
+
+        self.assertEqual(
+            1, len(self.portfolio_controller._portfolio._transactions)
+        )
+
+    def _check_buy_asset_exception(
+        self, test_asset: Dict, exception: Exception
+    ) -> None:
+        """Check if portoflio buy_asset raises provided exception,
+        also check if asset is not added into portfolio assets.
+        """
+
+        self.assertRaises(
+            exception,
+            self.portfolio_controller.buy_asset,
+            test_asset["name"],
+            test_asset["unit_price"],
+            test_asset["amount"],
+            test_asset["currency"],
+        )
+
+        assets = self.portfolio_controller._portfolio._assets
+
+        # check if asset was not added to assets
+        self.assertNotIn(test_asset["name"], assets.keys())
+
+        # check if transaction record was not created
+        self.assertEqual(
+            0, len(self.portfolio_controller._portfolio._transactions)
+        )
+
+    def test_buy_example(self) -> None:
+        """buy asset with provided valid arguments
+
+        Should add an asset to portfolio with values provided in arguments.
+        And create a transaction record in transaction history
+        """
+
+        test_asset = self.asset_template
+        self._check_buy_valid_asset(test_asset)
+        self._clear_transactions()
+
+    def test_buy_empty_name(self) -> None:
+        """Buy asset with empty string as name
+
+        Should raise ValueError
+        """
+
+        test_asset = self.asset_template
+        test_asset["name"] = ""
+
+        self._check_buy_asset_exception(test_asset, ValueError)
+        self._clear_transactions()
+
+    def test_buy_negative_unitprice(self) -> None:
+        """Buy asset with negative unit price
+
+        Should raise ValueError
+        """
+
+        test_asset = self.asset_template
+        test_asset["unit_price"] = -1
+
+        self._check_buy_asset_exception(test_asset, ValueError)
+        self._clear_transactions()
+
+    def test_buy_negative_amount(self) -> None:
+        """Buy negative amount of asset
+
+        Should raise ValueError
+        """
+
+        test_asset = self.asset_template
+        test_asset["amount"] = -10
+        self._check_buy_asset_exception(test_asset, ValueError)
+        self._clear_transactions()
+
+    def test_buy_empty_currency(self) -> None:
+        """Buy an asset with empty string as currency
+
+        Should raise ValueError
+        """
+
+        test_asset = self.asset_template
+        test_asset["currency"] = ""
+        self._check_buy_asset_exception(test_asset, ValueError)
+        self._clear_transactions()
+
+
 if __name__ == "__main__":
     unittest.main()
